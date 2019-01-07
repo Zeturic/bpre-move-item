09d95816f4012035dbd86e137db6b5b019c52a2a
Kevin Mills <kevin.mills226@gmail.com>
2019-01-06 21:19:41 -0500

    Added sub_81224B4

--- functions.s
+++ functions.s
@@ -15,6 +15,8 @@
 .definelabel sub_8123270, 0x08123270
 .definelabel sub_81231E8, 0x081231E8
 .definelabel sub_811F234, 0x0811F234
+.definelabel sub_81B469C, 0x081B469C
+.definelabel sub_81224B4, 0x081224B4
 
 .definelabel fdecoder, 0x08008FCC
 .definelabel pokemon_getnick, 0x081202E0
