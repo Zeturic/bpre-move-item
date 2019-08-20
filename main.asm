a244959fb2ab45521ff60ab7f2bda426d2c43741
Kevin Mills <kevin.mills226@gmail.com>
2019-08-19 21:22:43 -0400

    go back to always displaying MOVE

--- main.asm
+++ main.asm
@@ -17,32 +17,26 @@
 
     .arm
     .include "strings.asm"
-
-    .align 2
-    // expanded ITEM menu window template
-    gUnknown_CustomWindowTemplate:
-        window_template 0x02, 0x16, 0x0B, 0x07, 0x08, 0x0E, 0x373
     .thumb
+
+    gUnknown_ItemMenuActions:
+        .byte MENU_GIVE, MENU_TAKE_ITEM, MENU_MOVE, MENU_CANCEL2
 .endarea
+
+// expanded ITEM menu window template
+.org gUnknown_0845A148
+    window_template 0x02, 0x16, 0x0B, 0x07, 0x08, 0x0E, 0x373
+
+.org sListSizeTable + 8
+.byte 4
+
+.org sActionTable + 8 * 4
+.word gUnknown_ItemMenuActions
 
 .org sCursorOptions + MENU_MOVE * 8
 .word gText_Move, CursorCb_MoveItem |1
 
 .org sActionStringTable + MSG_MOVE * 4
 .word gText_MoveWhere
-
-.org 0x081236A0     // CursorCb_Item
-.area 0x64, 0xFE
-    ldr r3, =CursorCb_Item |1
-    bx r3
-    .pool
-.endarea
-
-.org 0x08121E5C     // sub_8121E5C
-.area 0x164, 0xFE
-    ldr r3, =sub_8121E5C |1
-    bx r3
-    .pool
-.endarea
 
 .close
