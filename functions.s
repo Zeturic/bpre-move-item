52482b847272e5a64b8502681c86bd24bc355e51
Kevin Mills <kevin.mills226@gmail.com>
2019-08-21 00:58:26 -0400

    cleanup functions.s

--- functions.s
+++ functions.s
@@ -1,46 +1,20 @@
-.definelabel PlaySE, 0x080722CC
-.definelabel sub_8121CE4, 0x08121CE4
-.definelabel sub_811F818, 0x0811F818
-
-.definelabel PartyMenuButtonHandler, 0x0811FEFC
-.definelabel sub_811FD88, 0x0811FD88
-.definelabel sub_8124258, 0x08124258
-.definelabel sub_811FBC4, 0x0811FBC4
-.definelabel sub_8122E5C, 0x08122E5C
-.definelabel some_other_kind_of_link_test, 0x080BF748
-.definelabel get_pokesel, 0x0811FBA4
-.definelabel sub_8123BF0, 0x08123BF0
-.definelabel sub_81202F8, 0x081202F8
-.definelabel sub_8123270, 0x08123270
-.definelabel sub_81231E8, 0x081231E8
-.definelabel DisplayPartyPokemonData, 0x0811F234
-.definelabel sub_81224B4, 0x081224B4
-.definelabel sub_812299C, 0x0812299C
-// .definelabel sub_8121E5C, 0x08121E5C
-.definelabel display_pokemon_menu_message, 0x08121D0C
-.definelabel sub_811FB28, 0x0811FB28
-
+.definelabel StringAppend, 0x08008DA4
 .definelabel StringExpandPlaceholders, 0x08008FCC
-.definelabel GetMonNickname, 0x081202E0
-.definelabel schedule_bg_copy_tilemap_to_vram, 0x080F67A4
-.definelabel RenderPartyMenuBox, 0x0811F124
-.definelabel InitPartyMenu, 0x0811EA44
-.definelabel AddTextPrinterParameterized3, 0x0812E51C
-.definelabel HandleMenuInput, 0x08122C5C
-.definelabel AppendToList, 0x0806FEC8
-.definelabel AddWindow, 0x08003CE4
-.definelabel sub_8121DF8, 0x08121DF8
-.definelabel AddTextPrinterParameterized, 0x08002C48
-.definelabel DrawStdFrameWithCustomTileAndPalette, 0x0810F2E8
-.definelabel CopyItemName, 0x08099E90
 
 .definelabel GetMonData, 0x0803FBE8
 .definelabel SetMonData, 0x0804037C
 
-.definelabel memcpy, 0x081E5E78
-.definelabel GetMenuCursorDimensionByFont, 0x080F7AC0
-.definelabel GetFontAttribute, 0x080F79D8
-.definelabel AddTextPrinterParameterized4, 0x0812E5A4
-.definelabel choice_setup, 0x0810F774
-.definelabel SetWindowTemplateFields, 0x0810FE50
-.definelabel StringAppend, 0x08008DA4
+.definelabel PlaySE, 0x080722CC
+.definelabel CopyItemName, 0x08099E90
+.definelabel some_other_kind_of_link_test, 0x080BF748
+.definelabel schedule_bg_copy_tilemap_to_vram, 0x080F67A4
+
+.definelabel sub_811F818, 0x0811F818
+.definelabel sub_811FD88, 0x0811FD88
+.definelabel PartyMenuButtonHandler, 0x0811FEFC
+.definelabel GetMonNickname, 0x081202E0
+.definelabel sub_81202F8, 0x081202F8
+.definelabel display_pokemon_menu_message, 0x08121D0C
+.definelabel sub_8121CE4, 0x08121CE4
+.definelabel sub_81224B4, 0x081224B4
+.definelabel sub_8123BF0, 0x08123BF0
--- src/functions.h
+++ src/functions.h
@@ -1,23 +1,23 @@
 #pragma once
 #include "types.h"
 
-void PlaySE(u16 songNum);
-void display_pokemon_menu_message(u32 stringID);
-void sub_811F818(u8 slot, u8 b);
-bool8 some_other_kind_of_link_test(void);
-u16 PartyMenuButtonHandler(s8 *ptr);
-void sub_811FD88(u8 taskId, s8 *ptr);
-u8 sub_81202F8(const u8* str, u8 b);
-void sub_8123BF0(u8 taskId);
-
-u8* GetMonNickname(struct Pokemon *mon, u8 *dest);
-u8 *StringExpandPlaceholders(u8 *dest, const u8 *src);
-void schedule_bg_copy_tilemap_to_vram(u8 bgNum);
-void sub_81224B4(struct Pokemon *mon, struct Struct203B0B4 *ptr);
 u8 *StringAppend(u8 *dest, const u8 *src);
-void CopyItemName(u16 itemId, u8 *dst);
+u8 *StringExpandPlaceholders(u8 *dest, const u8 *src);
 
 u32 GetMonData(struct Pokemon *mon, s32 field);
 void SetMonData(struct Pokemon *mon, s32 field, const void *dataArg);
 
+void PlaySE(u16 songNum);
+void CopyItemName(u16 itemId, u8 *dst);
+bool8 some_other_kind_of_link_test(void);
+void schedule_bg_copy_tilemap_to_vram(u8 bgNum);
+
+void sub_811F818(u8 slot, u8 b);
+void sub_811FD88(u8 taskId, s8 *ptr);
+u16 PartyMenuButtonHandler(s8 *ptr);
+u8* GetMonNickname(struct Pokemon *mon, u8 *dest);
+u8 sub_81202F8(const u8* str, u8 b);
+void display_pokemon_menu_message(u32 stringID);
 void sub_8121CE4(u8 *ptr);
+void sub_81224B4(struct Pokemon *mon, struct Struct203B0B4 *ptr);
+void sub_8123BF0(u8 taskId);
